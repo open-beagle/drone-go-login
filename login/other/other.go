2baad8b3614bd10411a6a7e7d7cb89463c17a79e
luzhijian <luzhijian@wodcloud.com>
2022-09-14 10:00:40 +0800

    add other oauth2 login

---
+++ login/other/other.go
@@ -0,0 +1,41 @@
+// Copyright 2017 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package other
+
+import (
+	"net/http"
+
+	"github.com/drone/go-login/login"
+	"github.com/drone/go-login/login/internal/oauth2"
+)
+
+var _ login.Middleware = (*Config)(nil)
+
+// Config configures the GitLab auth provider.
+type Config struct {
+	ClientID     string
+	ClientSecret string
+	RedirectURL  string
+	Server       string
+	Scope        []string
+	Client       *http.Client
+}
+
+// Handler returns a http.Handler that runs h at the
+// completion of the GitLab authorization flow. The GitLab
+// authorization details are available to h in the
+// http.Request context.
+func (c *Config) Handler(h http.Handler) http.Handler {
+	return oauth2.Handler(h, &oauth2.Config{
+		BasicAuthOff:     true,
+		Client:           c.Client,
+		ClientID:         c.ClientID,
+		ClientSecret:     c.ClientSecret,
+		RedirectURL:      c.RedirectURL,
+		AccessTokenURL:   c.Server + "/oauth/token",
+		AuthorizationURL: c.Server + "/oauth/authorize",
+		Scope:            c.Scope,
+	})
+}
